1c6544e638342a5ceff87560f049066fad5254ef
safwanvk <safwanvalakundil@gmail.com>
2020-11-24 17:50:27 +0530

    :zap: Add order system

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import MenuItem, Category, OrderModel
+
+admin.site.register(MenuItem)
+admin.site.register(Category)
+admin.site.register(OrderModel)
--- customer/urls.py
+++ customer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import Order
 
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
-    path('about', views.About.as_view(), name="about")
+    path('about', views.About.as_view(), name="about"),
+    path('order/', Order.as_view(), name='order'),
 ]
