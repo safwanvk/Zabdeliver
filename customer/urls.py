a9008592358c6d4fb3f36381ea57f8e75859824c
safwanvk <safwanvalakundil@gmail.com>
2020-11-25 14:40:46 +0530

    :zap: Add html for order pay cofirm

--- customer/urls.py
+++ customer/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import Order
+from .views import Order, OrderConfirmation
 
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
     path('about', views.About.as_view(), name="about"),
     path('order/', Order.as_view(), name='order'),
+    path('order-confirmation/<int:pk>', OrderConfirmation.as_view(),
+         name='order-confirmation')
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,6 @@
 from django.core.mail import send_mail
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-
 
 # Create your views here.
 from .models import MenuItem, OrderModel
@@ -102,4 +101,25 @@ class Order(View):
             'price': price
         }
 
+        return redirect('order-confirmation', pk=order.pk)
+
+
+class OrderConfirmation(View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+
+        context = {
+            'pk': order.pk,
+            'items': order.items,
+            'price': order.price,
+        }
+
         return render(request, 'customer/order_confirmation.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        pass
+
+
+class OrderPayConfirmation(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/order_pay_confirmation.html')
